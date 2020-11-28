4851a7c30f4dd069cfcc4ff8371eed1a78c8b46b
LiWenBo <liwenbo4922@163.com>
2020-11-28 16:11:39 +0800

    Initial commit

---
+++ src/main/java/com/li/annotation_reflection/AnnotationReflectionApplication.java
@@ -0,0 +1,13 @@
+package com.li.annotation_reflection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationReflectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnotationReflectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/li/annotation_reflection/AnnotationReflectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.li.annotation_reflection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotationReflectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
